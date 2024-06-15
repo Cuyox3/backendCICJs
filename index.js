456cbbfadf81e3ee136a4e00ad64836584969e4c
Cuyox3 <artuciiz@gmail.com>
2024-06-15 15:44:51 -0600

    Add git ignore modules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { constrainedMemory } = require('process');
 const app = express();
 
 const port = 3000
@@ -24,9 +25,26 @@ app.get('/clients', (req, res) =>  {
 app.post('/clients', (req, res) =>  {
     const info = req.body;
     const clientsAmount = (Object.keys(clients).length + 1);
+    console.log(clientsAmount);
     clients [clientsAmount] = info;
+    console.log(clients);
     res.status(201).json({
         clients,
         message: "Clients list" 
+    });
+});
+
+
+app.patch('/clients/update_first_name/:id', (req, res) =>  {
+    const { id }  = req.params.id; //Destructuring
+    const { first_name } = req.body;
+    clients[id].first_name = first_name;
+    const clientsAmount = (Object.keys(clients).length + 1);
+    console.log(clientsAmount);
+    clients [clientsAmount] = info;
+    console.log(clients);
+    res.status(201).json({
+        clients,
+        message: "Clients list updating " 
     });
 });
