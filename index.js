39b4def00551b0e4db34b86cd0870f44e52f1a2b
Cuyox3 <artuciiz@gmail.com>
2024-06-15 17:57:48 -0600

    middle class

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+const exp = require('constants');
 const express = require('express');
 const { constrainedMemory } = require('process');
 const app = express();
 
-const port = 3000
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use((req,res,next) => {
+    console.log('middleware');
+    next();
+});
 
 app.listen(port, () => {
     console.log('server running on',port);
 });
 
+app.get('/clients', (req, res,next) =>  {
+    console.log('middleware');
+    next();
+});
 
 const clients = {
+    c1:{
     first_name: "Juan",
     last_name: "Perez",
     spent_amount: 100
+    }
 }
+
 
 app.get('/clients', (req, res) =>  {
     res.status(200).json({
@@ -33,18 +47,39 @@ app.post('/clients', (req, res) =>  {
         message: "Clients list" 
     });
 });
+app.post('/clients/:id', 
+    localMv,
+    (req, res) =>  {
+    const cientId = req.body;
+    const client= clients[clientId];
+    console.log(client);
+    res.status(200).json({
+        clients,
+        message: "Here the client by id" 
+    });
+});
 
+
+// Falta por completar
 
 app.patch('/clients/update_first_name/:id', (req, res) =>  {
     const { id }  = req.params.id; //Destructuring
     const { first_name } = req.body;
     clients[id].first_name = first_name;
-    const clientsAmount = (Object.keys(clients).length + 1);
-    console.log(clientsAmount);
-    clients [clientsAmount] = info;
-    console.log(clients);
-    res.status(201).json({
+
+    res.status(200).json({
         clients,
         message: "Clients list updating " 
+    });
+});
+
+app.delete('/clients/:id', (req, res) =>  {
+    const { id }  = req.params.id; //Destructuring
+    delete clients[id];
+    clients[id].first_name = first_name;
+
+    res.status(200).json({
+        clients,
+        message: "Clients list delete  " 
     });
 });
